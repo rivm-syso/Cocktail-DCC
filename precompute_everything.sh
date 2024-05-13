7288f2df724b6af0e87a51f7ca9bc6e18a18b050
Aldo Hennink <aldo.hennink@vortech.nl>
2024-05-13 12:08:01 +0200

    update README: mention precompute_everything.sh + removed out-dated comments from precompute_everything.sh

--- precompute_everything.sh
+++ precompute_everything.sh
@@ -9,7 +9,3 @@ rm -rf matrices
 cmake -S . -B "${build_dir}" -DCMAKE_BUILD_TYPE="${build_type}"
 cmake --build "${build_dir}" --config "${build_type}"
 cmake --build "${build_dir}" --config "${build_type}" --target precompute_everything
-
-#In libpinpoint ln 68 the location to the matrices is to be set
-#in libdcc ln 195-199 is where the values for DCC values is given
-#in libdcc ln 116-125 is where the different locations for ground dcc can be given.
